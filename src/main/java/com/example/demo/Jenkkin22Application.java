7622cf79ffea208757636cdae0999a9a4e767fac
skdilshadbegum07@gmail.com <skdilshadbegum07@gmail.com>
2022-02-15 19:44:11 +0530

    jenfile

---
+++ src/main/java/com/example/demo/Jenkkin22Application.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+
+@RestController
+public class Jenkkin22Application {
+
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Spring Boot Docker Demo";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jenkkin22Application.class, args);
+	}
+
+	
+	
+}
